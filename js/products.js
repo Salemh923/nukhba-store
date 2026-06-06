b1b70a77ea58870af26baa49a9fd9370a2356bfc
Salemh923 <alkuntsa2@gmail.com>
2026-06-07 00:51:44 +0300

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,27 @@
+const products = [
+
+{
+id:1,
+title:"iPhone 15 Pro Max",
+condition:"مستعمل ممتاز",
+price:4199,
+image:"https://images.unsplash.com/photo-1695048133142-1a20484d2569"
+},
+
+{
+id:2,
+title:"Samsung S24 Ultra",
+condition:"جديد",
+price:4399,
+image:"https://images.unsplash.com/photo-1610945265064-0e34e5519bbf"
+},
+
+{
+id:3,
+title:"MacBook Air M2",
+condition:"مستعمل",
+price:3999,
+image:"https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
+}
+
+];
