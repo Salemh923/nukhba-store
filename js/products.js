0dfd972348eb13c52ed2014f72febac9cac5bd7f
Salemh923 <alkuntsa2@gmail.com>
2026-06-07 04:53:48 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,27 +1,12 @@
-const products = [
+document.addEventListener("DOMContentLoaded", function(){
+  const search = document.querySelector(".search");
 
-{
-id:1,
-title:"iPhone 15 Pro Max",
-condition:"مستعمل ممتاز",
-price:4199,
-image:"https://images.unsplash.com/photo-1695048133142-1a20484d2569"
-},
-
-{
-id:2,
-title:"Samsung S24 Ultra",
-condition:"جديد",
-price:4399,
-image:"https://images.unsplash.com/photo-1610945265064-0e34e5519bbf"
-},
-
-{
-id:3,
-title:"MacBook Air M2",
-condition:"مستعمل",
-price:3999,
-image:"https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
-}
-
-];
+  if(search){
+    search.addEventListener("input", function(){
+      const q = search.value.toLowerCase();
+      document.querySelectorAll(".card").forEach(card=>{
+        card.style.display = card.innerText.toLowerCase().includes(q) ? "block" : "none";
+      });
+    });
+  }
+});
