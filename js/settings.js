16468f81d331e3f4b3c021dbf5e7ac78aa377455
Salemh923 <alkuntsa2@gmail.com>
2026-06-08 05:06:37 +0300

    Create settings.js

---
+++ js/settings.js
@@ -0,0 +1,26 @@
+const STORE_SETTINGS = {
+  storeName: "نُخبة",
+  storeSubtitle: "جديد ومستعمل مضمون",
+  phone: "+966500000000",
+  whatsapp: "https://wa.me/966500000000",
+  email: "info@nukhba.sa",
+  city: "الرياض، المملكة العربية السعودية",
+
+  social: {
+    instagram: "https://www.instagram.com/yourstore",
+    tiktok: "https://www.tiktok.com/@yourstore",
+    x: "https://x.com/yourstore",
+    snapchat: "https://www.snapchat.com/add/yourstore",
+    youtube: "https://www.youtube.com/@yourstore"
+  },
+
+  payment: {
+    mada: true,
+    visa: true,
+    mastercard: true,
+    applePay: true,
+    stcPay: true,
+    cashOnDelivery: true,
+    bankTransfer: true
+  }
+};
